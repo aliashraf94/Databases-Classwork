a1ce09cdf0bf242c85ee3636ac044524020ff405
Ali Ashraf <adaydreamer10@gmail.com>
2021-06-19 17:33:51 +0200

    Inclass Practice

--- Week-1/week-1.sql
+++ Week-1/week-1.sql
@@ -17,36 +17,61 @@ CREATE TABLE customers (
 
     
 CREATE TABLE hotels (
-  id        SERIAL PRIMARY KEY,
-  name      VARCHAR(30) NOT NULL,
-  country     VARCHAR(30) NOT NULL,
-  city      VARCHAR(30) NOT NULL
+  id        	SERIAL PRIMARY KEY,
+  name      	VARCHAR(30) NOT NULL,
+  rooms    		INT NOT NULL,
+  postcode      VARCHAR(30) NOT NULL
 );
 
     
  CREATE TABLE rooms (
-  id        SERIAL PRIMARY KEY,
-  hotel_id INT REFERENCES hotels(id),
-  is_available boolean NOT null,
-  price_per_night INT NOT NULL
+  id        		SERIAL PRIMARY KEY,
+  hotel_id 			INT REFERENCES hotels(id),
+  is_available		boolean NOT null,
+  price_per_night 	INT NOT NULL
 );
 
  CREATE TABLE bank_details (
-  id        SERIAL PRIMARY KEY,
-  customer_id INT REFERENCES customers(id),
-  bank_name  VARCHAR(30) NOT null,
-  iban_code  VARCHAR(30) NOT null
+  id        	SERIAL PRIMARY KEY,
+  customer_id 	INT REFERENCES customers(id),
+  bank_name  	VARCHAR(30) NOT null,
+  iban_code  	VARCHAR(30) NOT null
 );
 
 
  CREATE TABLE bookings (
-  id        SERIAL PRIMARY KEY,
-  customer_id INT REFERENCES customers(id),
-  room_id INT REFERENCES rooms(id),
+  id        	SERIAL PRIMARY KEY,
+  customer_id 	INT REFERENCES customers(id),
+  hotel_id		INT REFERENCES hotels(id),
   check_in_date DATE NOT null,
-  number_of_nights INT NOT null
+  nights 		INT NOT null
 );
 
-insert into hotels ( name, country, city ) values ( 'H Hotel', 'Spain', 'Barcelona' );
-insert into hotels ( name, country, city ) values ( 'Eduard Hotels', 'Spain', 'Barcelona' );
+-- inserting the customer table
+INSERT INTO customers (name, email, address, city, postcode, country) VALUES ('John Smith','j.smith@johnsmith.org','11 New Road','Liverpool','L10 2AB','UK');
 
+-- Inserting the hotels table
+INSERT INTO hotels (name, rooms, postcode) VALUES ('Triple Point Hotel', 10, 'CM194JS');
+INSERT INTO hotels (name, rooms, postcode) VALUES ('Royal Cosmos Hotel', 5, 'TR209AX');
+INSERT INTO hotels (name, rooms, postcode) VALUES ('Pacific Petal Motel', 15, 'BN180TG');
+
+-- Inserting the bookings table
+INSERT INTO bookings (customer_id, hotel_id, check_in_date, nights) VALUES (1, 1, '2019-10-01', 2);
+
+-- receving all the data from tables
+SELECT * FROM customers;
+SELECT * FROM hotels;
+SELECT * from bookings;
+
+-- retrieve only name and address from coustomers table
+SELECT name,address FROM customers;
+
+-- retrieve all hotels having more than 7 rooms
+select * from hotels where rooms > 7;
+
+-- retrieve the customer name and address with id 1
+select  * from customers where id = 1 ;
+
+-- retrieve all the bookings starting after 2019/10/01
+-- retrieve all the bookings starting after 2019/10/01 for a minimum of 2 nights
+select  * from bookings where check_in_date > '2019/10/01' and nights >= 2;
